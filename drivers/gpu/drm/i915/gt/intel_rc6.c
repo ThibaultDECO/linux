97097d8c0938b38bc622143a6baf95c3c14e7ecb
ThibaultDECO <80053070+ThibaultDECO@users.noreply.github.com>
2025-11-15 15:21:06 +0100

    should be drm_warn not drm_notice

--- drivers/gpu/drm/i915/gt/intel_rc6.c
+++ drivers/gpu/drm/i915/gt/intel_rc6.c
@@ -546,7 +546,7 @@ static bool pctx_corrupted(struct intel_rc6 *rc6)
 	if (intel_uncore_read(rc6_to_uncore(rc6), GEN8_RC6_CTX_INFO))
 		return false;
 
-	drm_notice(&i915->drm,
+	drm_warn(&i915->drm,
 		   "RC6 context corruption, disabling runtime power management\n");
 	return true;
 }
